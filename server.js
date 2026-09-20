b9a0e51dbb73b581ca67b2abafd1cdf7bc025a0b
abhijit843pa-pixel <abhijit843pa@gmail.com>
2026-09-20 16:49:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ import express from "express";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://abhijit843pa-pixel.github.io");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 
 app.use(express.json());
 
